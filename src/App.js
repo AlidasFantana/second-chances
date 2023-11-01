6bcf97f8fbd500eafeb65c3c0dfb498efeb7d2f0
Alidas Fantana <alidasfantana@gmail.com>
2023-11-01 13:33:30 -0500

    Updated App.css and App.js with new design

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="linktree-container">
-      <div className="linktree-header">
-        <img src="path_to_your_logo.png" alt="Your Brand Logo" className="brand-logo" />
-        <h1>Your Brand Name</h1>
+    <div className="App">
+      <img id="userPhoto" src="https://avatars.githubusercontent.com/u/57337800?v=4" alt="User Photo" />
+      <a className="icon" href="https://codepen.io/Memet_" id="userName" target="_blank" rel="noopener noreferrer">
+        <h1><span>Zxce3</span></h1>
+      </a>
+      <div id="links">
+        <a className="link" href="https://instagram.com/secondchances.vtg" target="_blank" rel="noopener noreferrer"><i className="fa fa-github" style={{padding: '10px'}}></i>Github</a>
+        <a className="link" href="https://www.reddit.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-reddit" style={{padding: '10px'}}></i> Reddit </a>
+        <a className="link" href="https://www.depop.com/in/secondchances/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square" style={{padding: '10px'}}></i>Linkedin</a>
+        <a className="link" href="https://twitter.com/zxce3_" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter-square" style={{padding: '10px'}}></i> Twitter</a>
+        <a className="link" href="https://www.google.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram" style={{padding: '10px'}}></i>Instagram</a>
       </div>
-      <div className="linktree-links">
-        <a href="https://your-link-1.com" target="_blank" rel="noopener noreferrer" className="linktree-link">Link 1</a>
-        <a href="https://your-link-2.com" target="_blank" rel="noopener noreferrer" className="linktree-link">Link 2</a>
-        <a href="https://your-link-3.com" target="_blank" rel="noopener noreferrer" className="linktree-link">Link 3</a>
-        {/* Add more links as needed */}
-      </div>
-      <div className="linktree-footer">
-        <p>Follow us on social media!</p>
-        {/* Add social media icons/links here */}
+      <div className="Bottom-Section">
+        <p style={{color: 'wheat'}}>Thanks</p>
       </div>
     </div>
   );
